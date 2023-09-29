0a937f413d0fbb4e5a6019e19a29644fefd8d27a
Gatmach <123287171+Gatmach@users.noreply.github.com>
2023-09-29 13:54:57 +0300

    Basic calculator for adding two numbers

---
+++ SumCalculator.py
@@ -0,0 +1,11 @@
+''' Name: Gatmach Yuol Nyuon
+# Registration No: SCT211-0504/2021   
+'''
+print("Sum Calculator")
+name = input("What is your name?\n")
+print("Welcome to",name,"Electronic Calculator...")
+
+num1 =int(input("Please enter the first  digit : "))
+num2 = int(input("Please enter the second  digit : "))
+sum = num1 + num2
+print("Sum = ",sum)
